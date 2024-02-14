e4773eb508ce346b9549760f56fe9dc4c40b6c4a
Nathan Joel <nathanjoel1@hotmail.co.uk>
2024-02-14 12:13:22 +0000

    Testing a larger array

--- top-interview-questions/array/remove-duplicates/remove_duplicates.py
+++ top-interview-questions/array/remove-duplicates/remove_duplicates.py
@@ -14,7 +14,14 @@ class Solution(object):
 if __name__ == "__main__":
     # Example usage
     solution = Solution()
+    print("Test Case 1")
     nums = [1, 1, 2]
+    print("Original list:", nums)
+    new_length = solution.removeDuplicates(nums)
+    print("Modified list after removing duplicates:", nums[:new_length])
+    print("New length of the list:", new_length)
+    print("Test Case 2")
+    nums = [0,0,1,1,1,2,2,3,3,4]
     print("Original list:", nums)
     new_length = solution.removeDuplicates(nums)
     print("Modified list after removing duplicates:", nums[:new_length])
